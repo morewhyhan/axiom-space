b035badcf71f78a8dd2a071374c5ba07980ab9e5
morewhyhan <morewhyhan@gmail.com>
2026-06-06 23:54:21 +0800

    fix: use relative auth client path

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,6 @@
 import { createAuthClient } from "better-auth/react"
-import { getAuthUrl } from "./site-url"
 
 export const authClient = createAuthClient({
-  baseURL: typeof window !== "undefined" ? window.location.origin : getAuthUrl(),
   fetchOptions: {
     credentials: "include",
   },
