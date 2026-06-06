9fa8a54dc74a64d8020c9676e55ba0a2f5aa5893
morewhyhan <morewhyhan@gmail.com>
2026-06-06 23:44:35 +0800

    fix: keep auth client same-origin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { getAuthUrl } from "./site-url"
 
 export const authClient = createAuthClient({
-  baseURL: getAuthUrl(),
+  baseURL: typeof window !== "undefined" ? window.location.origin : getAuthUrl(),
   fetchOptions: {
     credentials: "include",
   },
